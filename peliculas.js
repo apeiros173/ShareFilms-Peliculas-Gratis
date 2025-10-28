bb924d3aa09b94e3757ea3d6c99e9ae504848e32
ShareFilmsOficial <sharefilmsoficial@gmail.com>
2025-10-28 11:26:20 -0600

    Create peliculas.js

---
+++ peliculas.js
@@ -0,0 +1,43 @@
+// peliculas.js
+const peliculas = [
+  {
+    id: "huye",
+    titulo: "¡Huye! (2017)",
+    descripcion:
+      "Chris va a conocer a los padres de Rose, su novia. Al principio se muestran encantadores, si bien un tanto nerviosos. Chris supone que se debe a que él es negro, pero un poco más tarde descubre que sus suegros ocultan un secreto espeluznante.",
+    genero: "Terror / Comedia / Suspenso",
+    duracion: "1h 44m",
+    imagen: "get-out.jpeg",
+    trailer: "https://drive.google.com/uc?export=preview&id=1en6HYU4w0GfvSf3PT8ZAqcxzbHvCcjHB",
+  },
+  {
+    id: "rango",
+    titulo: "Rango (2011)",
+    descripcion:
+      "Rango es un camaleón que lleva toda su vida viviendo como mascota en un terrario. Sin embargo, un buen día, mientras transportan su terrario, el recipiente se cae del auto en medio del desierto y acaba en un pueblo salvaje donde lo nombran alguacil.",
+    genero: "Infantil / Wéstern / Animación / Aventura",
+    duracion: "1h 47m",
+    imagen: "rango.jpeg",
+    trailer: "https://drive.google.com/uc?export=preview&id=1SB357lg3QIGL5RvdfrJQ53_1jmnHNv7u",
+  },
+  {
+    id: "spiderman-un-nuevo-universo",
+    titulo: "Spider-Man: Un nuevo universo (2018)",
+    descripcion:
+      "Luego de ser mordido por una araña radioactiva, el joven Miles Morales desarrolla misteriosos poderes que lo transforman en el Hombre Araña. Ahora deberá usar sus nuevas habilidades ante el malvado Kingpin, un enorme demente que puede abrir portales hacia otros universos.",
+    genero: "Animación / Acción / Ciencia Ficción / Superhéroes",
+    duracion: "1h 57m",
+    imagen: "into-the-spiderverse.jpeg",
+    trailer: "https://drive.google.com/uc?export=preview&id=1FTQh3ngY6CJmWgURjpCwuYHZMZWk-NY_",
+  },
+  {
+    id: "across-the-spiderverse",
+    titulo: "Spider-Man: A través del Spider-Verso (2023)",
+    descripcion:
+      "Después de reunirse con Gwen Stacy, el amigable vecino de tiempo completo de Brooklyn Spiderman es lanzado a través del multiverso, donde se encuentra a un equipo de gente araña encomendada con proteger su mera existencia.",
+    genero: "Acción / Ciencia ficción",
+    duracion: "2h 20m",
+    imagen: "across-the-spiderverse.jpeg",
+    trailer: "https://drive.google.com/uc?export=preview&id=1Gp-v1xvRausPDh9c88yxOSFusXM9dkwE",
+  },
+];
